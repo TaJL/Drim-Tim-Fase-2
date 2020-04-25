219bc39ae78b07bd40024e0d06aaf77a23121e0e
VG <vengadoravg@gmail.com>
2020-04-25 16:19:47 -0400

    mixing with colors and values

--- Assets/Scripts/Mixer.cs
+++ Assets/Scripts/Mixer.cs
@@ -4,6 +4,13 @@ using System.Collections.Generic;
 
 public class Mixer : MonoBehaviour {
   public SkinnedMeshRenderer skin;
+  public Dictionary<Reagent, float> content =
+    new Dictionary<Reagent, float>();
+  public Material material;
+
+  void OnEnable () {
+    material = skin.materials[0];
+  }
 
   void Update () {
     RaycastHit hit;
@@ -16,13 +23,22 @@ public class Mixer : MonoBehaviour {
       Bottle bottle = PlayerInteracter.Instance.target
         .GetComponentInChildren<Bottle>();
 
+      float delta = bottle.mixerFillSpeed * Time.deltaTime;
       float value =
-        Mathf.Clamp(skin.GetBlendShapeWeight(0) -
-                    bottle.mixerFillSpeed * Time.deltaTime,
-                    0, 100);
+        Mathf.Clamp(skin.GetBlendShapeWeight(0) - delta, 0, 100);
+
+      if (!content.ContainsKey(bottle.reagentData)) {
+        content[bottle.reagentData] = 0;
+      }
+
+      content[bottle.reagentData] += delta;
 
       skin.SetBlendShapeWeight(0, value);
 
+      material.color = new Color(0,0,0,0);
+      foreach (KeyValuePair<Reagent, float> entry in content) {
+        material.color += entry.Key.liquidColor * (entry.Value / (100 - value));
+      }
     }
   }
 }
---
+++ Assets/Scripts/Reagent.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Reagent {
+  public Color liquidColor = new Color(1,1,1,1);
+  public string reagentName;
+}
