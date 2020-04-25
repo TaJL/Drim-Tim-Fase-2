5d089adec9a3b627bc2659194b62b85661f552f1
VG <vengadoravg@gmail.com>
2020-04-25 09:40:37 -0400

    scriptables for feeding recipes db

--- Assets/Scripts/Recipe.cs
+++ Assets/Scripts/Recipe.cs
@@ -4,5 +4,6 @@ using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "Recipe", menuName = "Bar/Recipe")]
 public class Recipe : ScriptableObject {
+  public string NameOfDrink;
   public RequiredReagent[] reagents;
 }
