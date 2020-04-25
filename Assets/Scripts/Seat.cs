94bef1fe8cf618368b9a5d78d4d77e8f2515c633
Sehgel <lucasemanuelgonzalez8@gmail.com>
2020-04-25 16:59:02 -0300

    Added Clients, Bars, Seats and Client Manager

---
+++ Assets/Scripts/Seat.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seat
+{
+    public enum State
+    {
+        Free,
+        Ocuppied
+    }
+
+    public State current_state = State.Free;
+    public Vector3 position;
+
+    public Seat(Vector3 pos)
+    {
+        this.position = pos;
+    }
+
+    public bool IsFree
+    {
+        get
+        {
+            return current_state == State.Free;
+        }
+    }
+    public void Occupy()
+    {
+        current_state = State.Ocuppied;
+    }
+
+    public void Free()
+    {
+        current_state = State.Free;
+    }
+}
