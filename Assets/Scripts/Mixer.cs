0b559c53921e66721063b0a7ebada0bc0bfbec72
VG <vengadoravg@gmail.com>
2020-04-25 14:54:44 -0400

    mixer prefab

--- Assets/Scripts/Mixer.cs
+++ Assets/Scripts/Mixer.cs
@@ -8,7 +8,12 @@ public class Mixer : MonoBehaviour {
   void Update () {
     if (Input.GetMouseButton(0) &&
         PlayerInteracter.Instance.grabbing) {
-      
+      float fillSpeed = PlayerInteracter.Instance.target
+        .GetComponentInChildren<Bottle>().mixerFillSpeed;
+
+      skin.SetBlendShapeWeight(0, skin.GetBlendShapeWeight(0) +
+                               fillSpeed * Time.deltaTime);
+
     }
   }
 }
