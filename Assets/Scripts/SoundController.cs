819758f7ed756befdc76025977d22831e6f7a4ac
Tadeo <tadeojliuzzi@gmail.com>
2020-04-26 23:23:04 -0300

    recetas correjidas y un tema mas

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] audioClips;
+    [SerializeField] private AudioSource audioSource;
+
+    public void PlayClip(int clipNumber, float volume)
+    {
+        audioSource.PlayOneShot(audioClips[clipNumber], volume);
+    }
+}
