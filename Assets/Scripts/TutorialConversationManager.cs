c125d484e9c73e21f9f6e4468e0b583b44b27bf8
Tadeo <tadeojliuzzi@gmail.com>
2020-04-27 08:19:15 -0300

    varios postit, tutorial 1 y algunos tips

--- Assets/Scripts/TutorialConversationManager.cs
+++ Assets/Scripts/TutorialConversationManager.cs
@@ -6,7 +6,6 @@ public class TutorialConversationManager : MonoBehaviour {
   public static event System.Action<TutorialConversation> onConversationEnd;
 
   public UIConversationParticipant professor;
-  public UIConversationParticipant pupil;
   public const float LECTURE_TIME_PER_WORD = 0.5f;
 
   public void Display (TutorialConversation conversation) {
@@ -18,8 +17,6 @@ public class TutorialConversationManager : MonoBehaviour {
     for (int i=0; i<conversation.entries.Length; i++) {
       if (conversation.entries[i].author == ConversationParticipant.Professor) {
         yield return StartCoroutine(professor._Say(conversation.entries[i].message));
-      } else {
-        yield return StartCoroutine(pupil._Say(conversation.entries[i].message));
       }
     }
 
