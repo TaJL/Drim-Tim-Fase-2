1430c2e4acd02c9dc2e5291ed57c2436f52e5143
Tadeo <tadeo@ravegan.com>
2020-04-28 18:37:17 -0300

    correccion de sonido y edicion del las conversaciones

--- Assets/Scripts/TutorialConversationManager.cs
+++ Assets/Scripts/TutorialConversationManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using MEC;
 
 public class TutorialConversationManager : MonoBehaviour {
   public static event System.Action<TutorialConversation> onConversationEnd;
@@ -15,8 +16,13 @@ public class TutorialConversationManager : MonoBehaviour {
 
   IEnumerator _Display (TutorialConversation conversation) {
     for (int i=0; i<conversation.entries.Length; i++) {
+
       if (conversation.entries[i].author == ConversationParticipant.Professor) {
         yield return StartCoroutine(professor._Say(conversation.entries[i].message));
+      }
+      while (!Input.GetKeyDown(KeyCode.Space))
+      {
+        yield return Timing.WaitForOneFrame;
       }
     }
 
