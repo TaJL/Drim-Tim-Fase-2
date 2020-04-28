6daa5fe002d62845f6059e95004682b2800aa31b
VG <vengadoravg@gmail.com>
2020-04-28 19:41:11 -0400

    showing mouse back

--- Assets/Scripts/MouseOcluder.cs
+++ Assets/Scripts/MouseOcluder.cs
@@ -3,15 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class MouseOcluder : MonoBehaviour {
+  public bool showMouse = false;
+
+  void Start () {
+    if (showMouse) {
+      Display();
+    }
+
+    #if !UNITY_EDITOR
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
+    #endif
+  }
+
   void Update () {
-    if (Input.GetMouseButtonDown(0)) {
-      Cursor.lockState = CursorLockMode.Locked;
-      Cursor.visible = false;
+    if (Input.GetMouseButtonDown(0) && !showMouse) {
+      Hide();
     }
 
     if (Input.GetKeyDown(KeyCode.Escape)) {
-      Cursor.lockState = CursorLockMode.None;
-      Cursor.visible = true;
+      Display();
     }
+  }
+
+  public void Hide () {
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
+  }
+
+  public void Display () {
+    Cursor.lockState = CursorLockMode.None;
+    Cursor.visible = true;
   }
 }
