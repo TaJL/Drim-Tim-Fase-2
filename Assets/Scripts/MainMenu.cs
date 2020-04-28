ce769a877f64b8402604401eb9032f581dbd19a2
Sehgel <lucasemanuelgonzalez8@gmail.com>
2020-04-28 20:47:05 -0300

    Main menu animation and fixes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.anyKeyDown && !Input.GetMouseButtonDown(0) && !Input.GetMouseButtonDown(1) && !Input.GetMouseButtonDown(2))
         {
             SceneManager.LoadScene("Scenes/SampleScene");
         }
